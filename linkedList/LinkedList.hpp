50bba1d80f632039d594a79a2b8b9f683414a011
Joe Nordling <j822n038@ku.edu>
2021-02-14 22:34:34 -0600

    Added Search Function to LinkedList Class

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -37,10 +37,14 @@ bool LinkedList<T>::search(T value) const
 {
 	Node<T>* temp = m_front;
 	bool isFound = false;
+	while(isFound == false && temp != nullptr)
+	{
+		if(temp->getValue() == value){
+			isFound = true;
+		}
 
-	/** TODO 
-		Fix this method
-	*/
+		temp = temp->getNext();
+	}
 
 	return(isFound);
 }
